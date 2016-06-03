cc1a1a208cfd054414d678a9507db41378c1e9a9
Maldon <maldonmeehan@gmail.com>
2016-06-03 09:59:41 -0700

    add working id and js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,67 @@
 $(document).ready(function() {
-  $("form#dating").submit(function(event){
-
-    //$("#entertain")
-    var entertain = parseInt($("#entertain").val());
-
-    //$("#age")
-    var age = parseInt($("#age").val());
-
-    //$("#activity")
-    var activity = parseInt($("#activity").val());
+  $("form#survey").submit(function(event){
+    var room = parseInt($("#room").val());
+    var food = parseInt($("#food").val());
+    var weather = parseInt($("#weather").val());
+    var doors = parseInt($("#doors").val());
+    var location = parseInt($("#locaiton").val());
+    var transport = parseInt($("#transport").val());
 
 
-
-    if (entertain === 1 && age === 2 && activity === 1) {
-      $('#morrison').show();
-      $('#bieber').hide();
-      $('#mariah').hide();
-      $('#dicaprio').hide();
-      $('#sandler').hide();
-    } else if (animal === 2) {
-      $('#snake').show();
-      $('#turtle').hide();
-      $('#spider').hide();
-    } else {
-      (animal === 3)
-      $('#spider').show();
-      $('#snake').hide();
-      $('#turtle').hide();
+    if (room === 1 && food === 1 && weather === 1 && doors === 1) {
+      $('#home').show();
+      $('#glamping').hide();
+      $('#mountian').hide();
+      $('#egypt').hide();
+      $('#vegas').hide();
+      $('#borabora').hide();
+    } else if (room === 2 && food === 2 && weather === 2 && doors === 2 ) {
+      $('#glamping').show();
+      $('#home').hide();
+      $('#mountian').hide();
+      $('#egypt').hide();
+      $('#vegas').hide();
+      $('#borabora').hide();
+    } else if (room === 1 && food === 2 && weather === 2 && doors === 2 )  {
+      $('#mountian').show();
+      $('#glamping').hide();
+      $('#home').hide();
+      $('#egypt').hide();
+      $('#vegas').hide();
+      $('#borabora').hide();
     }
-
+    else if (room === 2 && food === 1 && weather === 2 && doors === 2 )  {
+      $('#egypt').show();
+      $('#glamping').hide();
+      $('#mountian').hide();
+      $('#home').hide();
+      $('#vegas').hide();
+      $('#borabora').hide();
+    }
+    else if (room === 2 && food === 2 && weather === 1 && doors === 2 )  {
+      $('#vegas').show();
+      $('#glamping').hide();
+      $('#mountian').hide();
+      $('#egypt').hide();
+      $('#home').hide();
+      $('#borabora').hide();
+    }
+    else if (room === 2 && food === 2 && weather === 2 && doors === 1 )  {
+      $('#borabora').show();
+      $('#glamping').hide();
+      $('#mountian').hide();
+      $('#egypt').hide();
+      $('#vegas').hide();
+      $('#home').hide();
+    }
+    else if (result === 6) {
+      $('#home').show();
+      $('#glamping').hide();
+      $('#mountian').hide();
+      $('#egypt').hide();
+      $('#vegas').hide();
+      $('#borabora').hide();
+    }
     event.preventDefault();
   });
 });
